878d7f5c7a2c085f05b49b27253164451573bb68
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-06 16:40:56 +0100

    Update users.js

--- src/api/users.js
+++ src/api/users.js
@@ -3,20 +3,20 @@ import api from "./client";
 // NOTE : adapte si besoin aux champs attendus par ton UtilisateurController
 
 export const fetchUsers = async () => {
-  const res = await api.get("/api/utilisateurs");
+  const res = await api.get("/utilisateurs");
   return res.data;
 };
 
 export const createUser = async (payload) => {
-  const res = await api.post("/api/utilisateurs", payload);
+  const res = await api.post("/utilisateurs", payload);
   return res.data;
 };
 
 export const updateUser = async (id, payload) => {
-  const res = await api.put(`/api/utilisateurs/${id}`, payload);
+  const res = await api.put(`/utilisateurs/${id}`, payload);
   return res.data;
 };
 
 export const deleteUser = async (id) => {
-  await api.delete(`/api/utilisateurs/${id}`);
+  await api.delete(`/utilisateurs/${id}`);
 };
