f4271055842d860e39f035c713eb4edb978aa602
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-06 17:23:47 +0100

    Add files via upload

---
+++ src/data/ModelData.jsx
@@ -0,0 +1,56 @@
+export const ModelData = [
+  {
+    id: "PC",
+    label: "Ordinateur",
+    objectName: "Cube005",
+    image: "/images/1.png"
+  },
+  {
+    id: "tableau",
+    label: "Tableau",
+    objectName: "Cube006",
+    image: "/images/2.png"
+  },
+  {
+    id: "Chaise",
+    label: "Chaise",
+    objectName: "Rectangle053",
+    image: "/images/9.png"
+  },
+  {
+    id: "Enceinte",
+    label: "Enceinte",
+    objectName: "Cube030",
+    image: "/images/4.png"
+  },
+  {
+    id: "Bureau",
+    label: "Bureau",
+    objectName: "Rectangle005",
+    image: "/images/10.png"
+  },
+  {
+    id: "Fenetre",
+    label: "Fenètre",
+    objectName: "Mur002",
+    image: "/images/1.png"
+  },
+  {
+    id: "Video-proj",
+    label: "Vidéo Projecteur",
+    objectName: "Cube049",
+    image: "/images/2.png"
+  },
+  {
+    id: "Lumiere",
+    label: "Lumière LED",
+    objectName: "Lumière_salle007",
+    image: "/images/8.png"
+  },
+  {
+    id: "Prise",
+    label: "Prises",
+    objectName: "Cube008",
+    image: "/images/10.png"
+  }
+];
---
+++ src/data/ModelDataVisi.jsx
@@ -0,0 +1,19 @@
+// src/data/ModelDataVisi.jsx
+
+export const ModelDataVisi = [
+  {
+    id: "PC",
+    label: "chaise",
+    objectNames: ["Plane021"], // tableau maintenant
+  },
+  {
+    id: "tableau",
+    label: "pano",
+    objectNames: ["panotroupla", "panotroumur"],
+  },
+  {
+    id: "test",
+    label: "test",
+    objectNames: ["Rectangle053"],
+  }
+];
