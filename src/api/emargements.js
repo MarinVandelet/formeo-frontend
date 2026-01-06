276a7041572aefba429e1e897261aa468be894ad
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-06 16:39:15 +0100

    Update emargements.js

--- src/api/emargements.js
+++ src/api/emargements.js
@@ -2,19 +2,19 @@ import api from "./client";
 
 // élève : émarger avec signature
 export const pointerEmargement = async (sessionId, signatureBase64) => {
-  const res = await api.post("/api/emargements", { sessionId, signatureBase64 });
+  const res = await api.post("/emargements", { sessionId, signatureBase64 });
   return res.data;
 };
 
 export const fetchMyEmargements = async () => {
-  const res = await api.get("/api/emargements/me");
+  const res = await api.get("/emargements/me");
   return res.data;
 };
 
 // intervenant / admin
 export const fetchFeuilleEmargement = async (sessionId, jourCours) => {
   const res = await api.get(
-    `/api/emargements/session/${sessionId}/jour/${jourCours}`
+    `/emargements/session/${sessionId}/jour/${jourCours}`
   );
   return res.data;
 };
