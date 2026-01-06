d758720511ff08b7a89065ad103a9428d408f80c
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-06 16:39:46 +0100

    Update formations.js

--- src/api/formations.js
+++ src/api/formations.js
@@ -1,27 +1,27 @@
 import api from "./client";
 
 export const fetchFormations = async () => {
-  const res = await api.get("/api/formations");
+  const res = await api.get("/formations");
   return res.data;
 };
 
 export const fetchFormationsByCategorie = async (categorieId) => {
-  const res = await api.get(`/api/formations`, {
+  const res = await api.get(`/formations`, {
     params: { categorieId },
   });
   return res.data;
 };
 
 export const createFormation = async (payload) => {
-  const res = await api.post("/api/formations", payload);
+  const res = await api.post("/formations", payload);
   return res.data;
 };
 
 export const updateFormation = async (id, payload) => {
-  const res = await api.put(`/api/formations/${id}`, payload);
+  const res = await api.put(`/formations/${id}`, payload);
   return res.data;
 };
 
 export const deleteFormation = async (id) => {
-  await api.delete(`/api/formations/${id}`);
+  await api.delete(`/formations/${id}`);
 };
