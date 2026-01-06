361022d38d44cd688da12c0a38427d014ce8c368
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-06 16:39:29 +0100

    Update evaluations.js

--- src/api/evaluations.js
+++ src/api/evaluations.js
@@ -3,7 +3,7 @@ import api from "./client";
 // Récupérer la "feuille d'évaluation" d'une session
 // -> [{ inscriptionId, utilisateurId, prenom, nom, email, note, dateEvaluation }]
 export const fetchEvaluationsBySession = async (sessionId) => {
-  const res = await api.get(`/api/evaluations/session/${sessionId}`);
+  const res = await api.get(`/evaluations/session/${sessionId}`);
   return res.data;
 };
 
@@ -11,7 +11,7 @@ export const fetchEvaluationsBySession = async (sessionId) => {
 // evaluations = [{ inscriptionId, note }]
 export const saveEvaluationsForSession = async (sessionId, evaluations) => {
   const res = await api.post(
-    `/api/evaluations/session/${sessionId}`,
+    `/evaluations/session/${sessionId}`,
     evaluations
   );
   return res.data;
