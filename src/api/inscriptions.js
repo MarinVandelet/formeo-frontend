771efc0d4a0abfab10c6bfa443632fc04e3feb2e
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-06 16:39:59 +0100

    Update inscriptions.js

--- src/api/inscriptions.js
+++ src/api/inscriptions.js
@@ -1,17 +1,17 @@
 import api from "./client";
 
 export const createInscription = async (sessionId) => {
-  const res = await api.post("/api/inscriptions", { sessionId });
+  const res = await api.post("/inscriptions", { sessionId });
   return res.data;
 };
 
 export const fetchMyInscriptions = async () => {
-  const res = await api.get("/api/inscriptions/me");
+  const res = await api.get("/inscriptions/me");
   return res.data;
 };
 
 // "Paiement" d'une inscription (appelera l'endpoint de paiement / validation)
 export const payInscription = async (inscriptionId) => {
-  const res = await api.post(`/api/inscriptions/${inscriptionId}/payer`);
+  const res = await api.post(`/inscriptions/${inscriptionId}/payer`);
   return res.data;
 };
