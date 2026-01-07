754463a57c61ba6c6b3701c183fe99da54a75729
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-08 00:55:58 +0100

    Update payment.js

--- src/api/payment.js
+++ src/api/payment.js
@@ -1,12 +1,10 @@
 import api from "./client";
 
-// crée une session Stripe Checkout
 export const createCheckoutSession = async (inscriptionId) => {
   const res = await api.post(`/paiement/checkout/${inscriptionId}`);
-  return res.data; // { sessionId, url }
+  return res.data; 
 };
 
-// confirme côté back (passe l'inscription à PAYEE)
 export const confirmPayment = async (inscriptionId) => {
   const res = await api.post(`/paiement/confirm/${inscriptionId}`);
   return res.data;
