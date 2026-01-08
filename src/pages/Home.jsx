1bee4ed7c3e0f9dfbf94717a1899f79a7a6470dc
Vandelet Marin <vandeletmarin.pro@gmail.com>
2026-01-08 01:13:17 +0100

    Refactor Home component structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,16 +21,14 @@ export default function Home() {
           transition-all duration-200
           hover:scale-105 hover:bg-indigo-100
           active:scale-95
-        "
-      >
+        ">
         Entrer dans la salle
       </button>
 
-      {/* Test Tailwind */}
       <div className="bg-red-500 text-white px-4 py-2 rounded">
         Tailwind OK
       </div>
-
     </div>
   );
 }
+
